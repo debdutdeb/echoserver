d72dd94b683a88916ce28b3b81350ba8cc5c5666
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2021-07-16 10:29:05 +0530

    initial push

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logrus.Info(r.URL.Path[1:])
+		fmt.Fprintf(w, "%q\n", r.URL.Path[1:])
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
